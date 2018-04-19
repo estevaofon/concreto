2a264f699b3ba5d9a4c9964d59c2904387900b98
Estevao Fonseca <estevaopfon@gmail.com>
2018-04-19 00:55:31 -0300

    Calcula da armadura de pele

--- viga-flexao.py
+++ viga-flexao.py
@@ -3,21 +3,23 @@ import math
 # bw h d -> mm
 bw = 150
 L = 6000
-h = L/(10)
-d = 0.9*h
+# h = L/(10)
+# d = 0.9*h
 # com d inicial
 # d = 400
+h = 650
+d = 590
 
 # espacamento -> mm
 cnom = 30
-dbrita = 25
+dbrita = 19
 dt = 6.3  # diametro do estribo
-bitola = 12.5
+bitola = 20
 
 # Mk -> kN.m
 M = 58.4
 # fck -> MPa
-fck = 25
+fck = 30
 fy = 500
 fyd = fy/1.15
 
@@ -35,7 +37,9 @@ bitola = bitola*10**-3
 
 # Calculos
 fcd = fck/1.4
-Msd = 1.4*M
+# Msd = 1.4*M
+Msd = 247
+Msd = Msd*10**3  # N.m
 d1_est = h-d
 
 
@@ -95,14 +99,16 @@ def dominio(kx):
 
 
 def desbitolagem(As):
-    As_cm = As * 10**4
+    # As_cm = As * 10**4
+    bitolas = [5, 6.3, 8, 10, 12.5, 16, 20, 22.0, 25.0, 32, 40]
+    bitolas_m = [bmm*10**-3 for bmm in bitolas]
     nbarras = {}
-    nbarras['10.0'] = As_cm/0.79
-    nbarras['12.5'] = As_cm/1.23
-    nbarras['16.0'] = As_cm/2.01
+    for bmm in bitolas_m:
+        area = math.pi*(bmm**2)/4
+        nbarras[str(bmm*10**3)] = As/area
     for key, value in nbarras.items():
         decimal = value - int(value)
-        if decimal <= 0.3:
+        if decimal <= 0.3 and value >= 1:
             nbarras[key] = math.floor(value)
         else:
             nbarras[key] = math.ceil(value)
@@ -168,7 +174,6 @@ def distribuicao_max(bw, nbarras, bitola, dt, dbrita, cnom):
     barra_unica = 0
     camadas = []
     n = nbarras[bitola_str]
-    print(f"{bwm}")
     barra_max = 0
     while bwm >= bw:
         n -= 1
@@ -176,7 +181,6 @@ def distribuicao_max(bw, nbarras, bitola, dt, dbrita, cnom):
         bwm = bwmin(12.5*10**-3, dt, dbrita, n, cnom)
         if bwm <= bw:
             barra_max = n
-        print(f"{bwm}")
     nb = nbarras[bitola_str]
     while nb > 0:
         if (nb - barra_max) >= 0:
@@ -206,12 +210,11 @@ def d_real(d1, h):
 
 def d_test(d1_est, d1_real):
     c = d1_real/d1_est
-    print(c)
     if c <= 1.10:
         print("d esta ok")
         return 1
     else:
-        print("Difereca maior que 1.10")
+        print("Difereca maior que 1.10 => {:.3f}".format(c))
         return 0
 
 
@@ -234,13 +237,69 @@ def delta_teste(cnom, dt, bitola, camadas, h):
         soma += barras*abitola*((bitola/2)+a*i+bitola*i)
     bitola_str = str(bitola*10**3)
     yc = soma/(nbarras[bitola_str]*abitola)
-    print(f"yc {yc*1000}")
     if yc <= 0.1*h:
-        print("Passou no delta teste")
+        print("Passou no teste de tensao centrada no cg")
     else:
         print("Não passou no delta teste")
     return 1
 
+
+def test_min_steelarea(bw, h, fck, As):
+    fck = str(int(fck/(10**6)))
+    tx_min = {'20': 0.15, '25': 0.15, '30': 0.15, '35': 0.164, '40': 0.179,
+              '45': 0.195, '50': 0.208, '55': 0.211, '60': 0.219, '65': 0.226,
+              '70': 0.233, '75': 0.239, '80': 0.245, '85': 0.251, '90': 0.256}
+    asmin = (tx_min[fck]/100)*bw*h
+    if As >= asmin:
+        print("Area min ok")
+        return 0
+    else:
+        print("Menor que a area min")
+        return 1
+
+
+def test_max_steelarea(bw, h, fck, As):
+    if As <= (4/100)*(bw*h):
+        print("Area maxima ok")
+        return 0
+    else:
+        print("Maior que a area maxima")
+        return 1
+
+
+def barra_int(value):
+    decimal = value - int(value)
+    if decimal <= 0.3 and value >= 1:
+        value = math.floor(value)
+    else:
+        value = math.ceil(value)
+    return value
+
+
+def as_pele(bw, h, bitola, dt, ev, camadas, cnom):
+    n_c = len(camadas)
+    n_ev = n_c - 1
+    w = cnom+dt+n_c*bitola+n_ev*ev-bitola/2
+    J = h - cnom - dt - w
+    n = 0
+    t = 0
+    if h > 0.6:
+        area_pele = (0.10/100)*bw*h
+        area_bitola = math.pi*(dt**2)/4
+        n = area_pele/area_bitola
+        n = barra_int(n)
+
+        t = J/(n+1)
+        if t > 0.2:
+            print("Nao ok, t > 20cm")
+        if t > d/3:
+            print("Nao ok, t > d/3")
+        if t > 15*bitola:
+            print("t não ok")
+
+    else:
+        print("Não precisa de As de pele")
+    return(n, t)
 
 
 kmd = kmd_calc(Msd, bw, d, fcd)
@@ -248,6 +307,8 @@ sigmac, lambdac = sigma_lambda_calc(fck)
 kx = kx_calc(kmd, sigmac, lambdac)
 kz = kz_calc(lambdac, kx)
 As = steel_area(Msd, d, fyd)
+amin = test_min_steelarea(bw, h, fck, As)
+amin = test_max_steelarea(bw, h, fck, As)
 As_cm = As * 10**4
 x1, x2 = linha_neutra(d, Msd, bw, fcd)
 x1_cm = x1 * 100
@@ -262,6 +323,9 @@ d1 = d1_real(cnom, dt, bitola, camadas)
 delta_teste(cnom, dt, bitola, camadas, h)
 d_r = d_real(d1, h)
 d_test(d1_est, d1)
+ev = ev_min(bitola, dbrita)
+n, t = as_pele(bw, h, bitola, dt, ev, camadas, cnom)
+print(f"dmin:{dminimo:.2f}")
 print(f"d1: {d1*100:.2f} cm")
 print(f"d1_est: {d1_est*100:.2f} cm")
 print(f"d_real: {d_r*100:.2f} cm")
@@ -269,10 +333,15 @@ print(f"kmd: {kmd:.3f}")
 print("kx: {:.3f}".format(kx))
 print("kz: {:.3f}".format(kz))
 print(f"x1:{x1_cm:.2f}; x2:{x2_cm:.2f}")
-print(f"dmin:{dminimo:.2f}")
 print(f"Dominio {dom}")
 print("As: {:.2f} cm2".format(As_cm))
-print(nbarras)
+selecionadas = [8.0, 10.0, 12.5, 16.0, 20.0, 22.0]
+dic_barras = {}
+for i in selecionadas:
+    dic_barras[str(i)] = nbarras[str(i)]
+print(dic_barras)
 print(f"Bitola:{str(bitola*10**3)}")
 print(f"camadas:{camadas}")
-print(f"eh:{[eh*100 for eh in eh_camadas]}")
+print(f"eh:{[str(round(eh*100,2)) for eh in eh_camadas]}")
+print(f"ev:{ev*100}")
+print(n, str(round(t*100,2)))
